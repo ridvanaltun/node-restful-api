a737c564f1cd89df70a480aa6acce2804fd4937b
Rıdvan Altun <ridvanaltun@outlook.com>
2020-02-11 23:11:08 +0300

    chore(tooling): fix conf for semantic-release

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    ['@semantic-release/git', {
+      assets: ['package.json', 'CHANGELOG.md'],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
+  ],
+};
