e05ed426910826a293f842b73988510c2087ab92
Rıdvan Altun <ridvanaltun@outlook.com>
2020-02-24 22:59:27 +0300

    fix(jwt): error handling and username validation

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -4,19 +4,26 @@ module.exports = (req, res, next) => {
   const token = req.headers['x-auth-token'];
   const {JWT_ACCESS_TOKEN_SECRET} = process.env;
   if (typeof token !== 'undefined') {
-    jwt.verify(token, JWT_ACCESS_TOKEN_SECRET, (err) => {
-      if (err) {
-        res.status(400);
-        res.send({code: 400, error: 'JWT token malformed'});
-        res.end();
+    jwt.verify(token, JWT_ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) return next(err);
+
+      if (req.params.username) {
+        if (req.params.username === decoded.username) {
+          req.username = decoded.username;
+          next();
+        } else {
+          res.status(400);
+          const message = 'This token not belong to ' + req.params.username;
+          res.send({code: 400, title: 'Bad Request', message});
+        }
       } else {
+        req.username = decoded.username;
         next();
       }
     });
   } else {
     // Forbidden
     res.status(403);
-    res.send({code: 403, error: 'Forbidden'});
-    res.end();
+    res.send({code: 403, title: 'Forbidden'});
   }
 };
